812b3ef6e18af78cc84736e5699947a973d89b35
NathanAtaliba <100451579+NathanAtaliba@users.noreply.github.com>
2023-11-13 23:17:03 -0300

    changes

--- projeto/cypress.config.js
+++ projeto/cypress.config.js
@@ -1,10 +1,9 @@
-import { defineConfig } from 'cypress';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      // implement node event listeners here
     },
   },
 });
